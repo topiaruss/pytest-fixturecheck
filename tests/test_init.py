af0a24aa5b564c2f1da4b3e53dde909fb2b0c671
Russ Ferriday <russ.ferriday@gmail.com>
2025-05-14 01:49:07 +0100

    Update version check in test_init.py to 0.3.3

--- tests/test_init.py
+++ tests/test_init.py
@@ -48,7 +48,7 @@ def test_exported_symbols():
 
     # Test version
     assert isinstance(__version__, str)
-    assert __version__ == "0.3.2"
+    assert __version__ == "0.3.3"
 
     # Test __all__ list
     expected_symbols = [
